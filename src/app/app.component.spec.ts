3ef3377744860d4f70268c5724ac7f5e8671d590
ChoeChangwon <qwefgh90@naver.com>
2019-01-25 14:25:44 +0900

    all tasks about #2 are done closes #2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent, minTermHeight, minTermWidth } from './app.component';
+import { ResizableModule } from 'angular-resizable-element';
+import { ResizeEvent } from 'angular-resizable-element';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ResizableModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +23,58 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'runjshell-web'`, () => {
+  it(`should have as title 'Online JShell'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('runjshell-web');
+    expect(app.title).toEqual('Online JShell');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to runjshell-web!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Online JShell!');
   });
+
+  it('should render #terminal element', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled: HTMLElement = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#terminal')).toBeDefined('Terminal doesn\'t exists');
+  });
+
+  it('should render textarea in Terminal', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    expect(app.term.textarea.textContent).toBeDefined("textaera should be a child of Terminal");
+  });
+
+  it('should check width and height exceed smallest size of each one', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    let left, top, width, height = 0;
+    const re: ResizeEvent = {rectangle: {left: left, top: top, width: width, height: height, bottom: undefined, right: undefined}, edges: undefined};
+    re.rectangle.width = minTermWidth;
+    re.rectangle.height = minTermHeight;
+    expect(app.validate(re)).toBe(true);
+    re.rectangle.width = minTermWidth - 1;
+    re.rectangle.height = minTermHeight - 1;
+    expect(app.validate(re)).toBe(false);
+  })
+
+  it('should make a terminal fit in dimension of div', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    app.onResizeEnd(0,0, minTermWidth, minTermHeight);
+    fixture.detectChanges();
+    let beforeRows = app.term.rows;
+    let beforeCols = app.term.cols;
+    app.onResizeEnd(0,0, minTermWidth+500, minTermHeight+500);
+    fixture.detectChanges();
+    let afterRows = app.term.rows;
+    let afterCols = app.term.cols;
+    expect(beforeRows).not.toBe(afterRows);
+    expect(beforeCols).not.toBe(afterCols);
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Terminal } from 'xterm';
+import * as fit from 'xterm/lib/addons/fit/fit';
+import { ResizeEvent } from 'angular-resizable-element';
+
+export const minTermWidth = 100;
+export const minTermHeight = 100;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'runjshell-web';
+export class AppComponent implements OnInit, AfterViewChecked {
+  title = 'Online JShell';
+  term: Terminal;
+  h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  style: object = { height: (this.h / 2) + 'px' };
+
+  /**
+   * When a dimension of div changes, fit a terminal in div.
+   */
+  ngAfterViewChecked() {
+    fit.fit(this.term);
+  }
+
+  /**
+   * It creates new terminal in #terminal.
+   */
+  ngOnInit() {
+    Terminal.applyAddon(fit);  // Apply the `fit` addon   
+    this.term = new Terminal();
+    this.term.open(document.getElementById('terminal'));
+    this.term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+  }
+
+  /**
+   * After user coordinate a size of terminal, it's called.
+   * @param left 
+   * @param top 
+   * @param width 
+   * @param height 
+   */
+  onResizeEnd(left: number, top: number, width: number, height: number): void {
+    this.style['left'] = `${left}px`;
+    this.style['top'] = `${top}px`;
+    this.style['width'] = `${width}px`;
+    this.style['height'] = `${height}px`;
+  }
+  /**
+   * Before onResizeEnd is called, it valiates a size to change.
+   * @param re dimension to be submitted from resizable stuff
+   */
+  validate(re: ResizeEvent){
+    let left = re.rectangle.left, top = re.rectangle.top, width = re.rectangle.width, height = re.rectangle.height;
+    if((width < minTermWidth) || (height < minTermHeight)){
+      return false;
+    }else return true;
+  }
 }
